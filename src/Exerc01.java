e45b70c36bc349604d12737e14f9a2fa549cc131
Thiago <thiago2b9s@gmail.com>
2025-03-17 03:53:27 -0300

    Exercicio 1

--- src/Exerc01.java
+++ src/Exerc01.java
@@ -1,5 +1,28 @@
+
+import java.util.Scanner;
+
 class Exerc01{
   public static void main (String[] args){
-    System.out.println("teste");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite o Valor de A: ");
+    int A = scanner.nextInt();
+
+    System.out.println("Digite o valor de B: ");
+    int B = scanner.nextInt();
+
+    System.out.println("Digite o valor de C ");
+    int C = scanner.nextInt();
+
+    int soma = A + B;
+
+    System.out.println("A soma de A + B = " + soma);
+    if (soma < C){
+      System.out.println("Soma menor que C");
+    } else {
+      System.out.println("Soma maior que C");
+    }
+
+    scanner.close();
   }
 }
